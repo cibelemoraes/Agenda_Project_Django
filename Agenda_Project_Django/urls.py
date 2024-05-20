cf70f2cdac6245b8cd6806b7d6eb267543d6f491
Your Name <you@example.com>
2024-05-19 21:19:14 -0300

    autenticação e login

--- Agenda_Project_Django/urls.py
+++ Agenda_Project_Django/urls.py
@@ -25,5 +25,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
